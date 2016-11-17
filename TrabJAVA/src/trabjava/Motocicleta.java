4a951d10ff9ec039b19afceba24329dededa0d3e
Joao Wind <jotahws.20@hotmail.com>
2016-11-17 21:25:36 -0200

    Classe do Diagrama com Getters, Setters, atributos e Construtor

--- TrabJAVA/src/trabjava/Motocicleta.java
+++ TrabJAVA/src/trabjava/Motocicleta.java
@@ -5,13 +5,20 @@
  */
 package trabjava;
 
+import java.util.Calendar;
+
 /**
  *
  * @author JotaWind
  */
-public class Motocicleta {
+public class Motocicleta extends Veiculo {
 
     private String modelo;
+
+    public Motocicleta(String modelo, Double valorDeCompra, String placa, int ano, Marca marca, Estado estado, Categoria categoria) {
+        super(valorDeCompra, placa, ano, marca, estado, categoria);
+        this.modelo = modelo;
+    }
 
     public String getModelo() {
         return modelo;
@@ -20,5 +27,35 @@ public class Motocicleta {
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
-    
+
+    @Override
+    public void locar(int dias, Calendar data, Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void vender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void devolver() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Locacao getLocacao() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getValorParaVenda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getValorDiariaLocacao() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
