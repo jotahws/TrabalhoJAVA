d19f6d80830ffa30bc3cdb0b29a8729c86fea616
Joao Wind <jotahws.20@hotmail.com>
2016-11-26 20:16:54 -0200

    oi russo

--- TrabJAVA/src/trabjava/Motocicleta.java
+++ TrabJAVA/src/trabjava/Motocicleta.java
@@ -25,56 +25,6 @@ public class Motocicleta extends Veiculo {
     }
 
     @Override
-    public void locar(int dias, Calendar data, Cliente cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void vender() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void devolver() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Estado getEstado() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Marca getMarca() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Categoria getCategoria() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Locacao getLocacao() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getPlaca() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int getAno() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public double getValorParaVenda() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
     public double getValorDiariaLocacao() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
--- TrabJAVA/src/trabjava/Veiculo.java
+++ TrabJAVA/src/trabjava/Veiculo.java
@@ -5,6 +5,8 @@
  */
 package trabjava;
 
+import java.util.Calendar;
+
 /**
  *
  * @author JotaWind
@@ -27,6 +29,62 @@ public abstract class Veiculo implements VeiculoI {
         this.estado = estado;
         this.categoria = categoria;
         this.locacao = null;
+    }
+
+    @Override
+    public void locar(int dias, Calendar data, Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void vender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void devolver() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Estado getEstado() {
+        return this.estado;
+    }
+
+    @Override
+    public Marca getMarca() {
+        return this.marca;
+    }
+
+    @Override
+    public Categoria getCategoria() {
+        return this.categoria;
+    }
+
+    @Override
+    public Locacao getLocacao() {
+        return this.locacao;
+    }
+
+    @Override
+    public String getPlaca() {
+        return this.placa;
+    }
+
+    @Override
+    public int getAno() {
+        return this.ano;
+    }
+
+    @Override
+    public double getValorParaVenda() {
+        Calendar c = Calendar.getInstance();
+        int idade = c.get(Calendar.YEAR) - this.ano;
+        Double valorVenda = this.valorDeCompra - (idade * 0.15 * this.valorDeCompra);
+        if (valorVenda < (this.valorDeCompra * 0.1)) {
+            return this.valorDeCompra * 0.1;
+        }
+        return valorVenda;
     }
 
 }
